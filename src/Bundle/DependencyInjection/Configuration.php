b78a7ae146c5ae544678b52adc500d8c9df6b511
Vivien DE BONA <vdebona@umanit.fr>
2025-10-17 14:04:32 +0200

    feat: update configuration logic
    - Namespace is required only for PHP storage
    - Add a name on each storage

--- src/Bundle/DependencyInjection/Configuration.php
+++ src/Bundle/DependencyInjection/Configuration.php
@@ -19,10 +19,27 @@ class Configuration implements ConfigurationInterface
             ->fixXmlConfig('storage', 'storages')
             ->children()
                 ->arrayNode('storages')
-                    ->addDefaultChildrenIfNoneSet()
+                    ->beforeNormalization()
+                        ->always(static function ($v) {
+                            foreach ($v as $key => $value) {
+                                if (!isset($value['name'])) {
+                                    $v[$key]['name'] = 'default';
+                                }
+                            }
+
+                            return $v;
+                        })
+                    ->end()
+                    ->addDefaultChildrenIfNoneSet('default')
+                    ->useAttributeAsKey('name')
                     ->arrayPrototype()
                         ->children()
+                            ->scalarNode('name')
+                                ->cannotBeEmpty()
+                                ->info('The name of the storage.')
+                            ->end()
                             ->enumNode('type')
+                                ->cannotBeEmpty()
                                 ->values([Storage::SQL_FILES->value, Storage::PHP_CLASSES->value])
                                 ->defaultValue(Storage::PHP_CLASSES->value)
                                 ->info('Determines whether triggers are stored in SQL (.sql files) or PHP (static functions).')
@@ -33,9 +50,21 @@ class Configuration implements ConfigurationInterface
                                 ->info('Determines the namespace for triggers classes.')
                             ->end()
                             ->scalarNode('directory')
+                                ->cannotBeEmpty()
                                 ->defaultValue('%kernel.project_dir%/triggers')
                                 ->info('Directory where triggers/functions are stored, depending on the selected type.')
                             ->end()
+                        ->end()
+                        ->validate()
+                            ->always(static function (array $v): array {
+                                $type = $v['type'] ?? Storage::PHP_CLASSES->value;
+
+                                if ($type === Storage::SQL_FILES->value) {
+                                    unset($v['namespace']);
+                                }
+
+                                return $v;
+                            })
                         ->end()
                     ->end()
                 ->end()
